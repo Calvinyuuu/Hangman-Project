936468a3bbf0eeb629a6dc310bced93c205660df
Calvinyuuu <yucalvin5@gmail.com>
2021-06-15 11:06:43 -0700

    version 2
    logic complete

--- js/core.js
+++ js/core.js
@@ -7,26 +7,27 @@ let isGameLive;
 let displayHint = document.getElementById("hint");
 let keyboard = document.getElementById("keyboard");
 let underscore = document.getElementById("underscore");
-let numberOfErrors = 6;
+const maxErrors = 6;
+let correctGuesses = 0;
+let incorrectGuesses = 0;
 
 class Game{
     constructor(){
         this.words = ["potato", "apple", "grape", "banana", "grapefruit"];
         this.hints = ["brown", "red", "purple", "yellow", "reddish"];
         numberOfLetters = [];
-        chooseWord(this.words);
+        chooseWord(this.words, this.hints);
         createButtons();
         replaceUnderscore();
-        runGame();
     }
 }
 
-function runGame(){
-}
-
-function chooseWord(words){
-    wordToBeGuessed = words[Math.floor(Math.random() * words.length)];
+function chooseWord(words, hints){
+    let chosenWordIndex = Math.floor(Math.random() * words.length)
+    wordToBeGuessed = words[chosenWordIndex];
+    displayHint.innerHTML = hints[chosenWordIndex];
     updateAnswer = wordToBeGuessed.split('').map(() => '_');
+    
 }
 
 function createButtons(){
@@ -48,15 +49,53 @@ function replaceUnderscore(){
 }
 
 function letterSelected(event){
+    let correctAnswer = false;
+    
     updateAnswer = wordToBeGuessed.split('').map((letterComparedTo, index) => {
         if(event.target.innerText === letterComparedTo){
+            correctAnswer = true;
+            correctGuesses++;
             return `${event.target.innerText}`;
-        };
+
+        }
         event.target.disabled = true;
         return updateAnswer[index];
     });
-
+    
+    if(correctAnswer != true){
+        incorrectGuesses++
+    }
+    console.log(incorrectGuesses + `incorrect`);
+    console.log(correctGuesses + `correct`);
     replaceUnderscore();
+    isGameValid();
+}
+
+function isGameValid(){
+    //if correctAnswer is at max
+    if(correctGuesses === wordToBeGuessed.length){
+        winGame();
+        console.log(`win`);
+    }
+    //if incorrectAnswer is at max
+    if(incorrectGuesses === maxErrors){
+        loseGame();
+        console.log(`lose`);
+    }
+}
+
+function winGame(){
+//display win message
+//display restart
+//goto restartGame()
+}
+function loseGame(){
+//display lose message
+//display restart
+//goto restartGame()
+}
+function restartGame(){
+
 }
 
 const game01 = new Game();
